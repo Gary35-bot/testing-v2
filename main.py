04221ffa3faa0e10acf7bc9f5a1e02e5d5b9f9ba
Lifechoices <gafrica851@gmail.com>
2021-06-08 10:26:05 +0200

    Adding mini calculator file.

---
+++ main.py
@@ -0,0 +1,13 @@
+class MyMiniCalculator():
+
+    def add(self, a, b):
+        return a + b
+
+    def subtract(self, a, b):
+        return a - b
+
+    def multiply(self, a, b):
+        return a * b
+
+    def divide(self, a, b):
+        return a / b
